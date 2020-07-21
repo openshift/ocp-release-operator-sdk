c395d03a3c4137e1bf60aec94a7453a699f21bf0
Fabian von Feilitzsch <fabian@fabianism.us>
2020-07-21 12:05:35 -0400

    Update install commands

--- ci/tests/e2e-ansible-features.sh
+++ ci/tests/e2e-ansible-features.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 
+set -ex
+
 source hack/lib/test_lib.sh
 
 header_text "Running molecule tests to check advanced Ansible features"
 
-yum install -y libffi-devel openssl-devel python3 python3-devel gcc python3-pip python3-setuptools
-pip3 install --user pyasn1==0.4.7 pyasn1-modules==0.2.6 idna==2.8 ipaddress==1.0.22
-pip3 install --user molecule==3.0.2
-pip3 install --user ansible-lint yamllint
-pip3 install --user docker openshift jmespath
+yum install -y \
+  libffi-devel \
+  openssl-devel \
+  python3 \
+  python3-devel \
+  gcc \
+  python3-pip \
+  python3-setuptools
+
+pip3 install \
+  pyasn1==0.4.7 \
+  pyasn1-modules==0.2.6 \
+  idna==2.8 \
+  ipaddress==1.0.22 \
+  molecule==3.0.2 \
+  ansible-lint \
+  yamllint \
+  docker \
+  openshift \
+  jmespath \
+  ansible
+
 ansible-galaxy collection install community.kubernetes
 
 cd test/ansible
