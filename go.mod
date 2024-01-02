b3dfd618a9e87db5aa4e37afa83f3b76ec435fc3
everettraven <everettraven@gmail.com>
2024-01-02 11:24:05 -0500

    go mod tidy + vendor
    Signed-off-by: everettraven <everettraven@gmail.com>

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/kr/text v0.2.0
 	github.com/markbates/inflect v1.0.4
 	github.com/maxbrunsfeld/counterfeiter/v6 v6.2.2
+	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/ginkgo/v2 v2.12.0
 	github.com/onsi/gomega v1.27.10
 	github.com/operator-framework/ansible-operator-plugins v1.33.0
@@ -174,6 +175,7 @@ require (
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc5 // indirect
 	github.com/opencontainers/runc v1.1.9 // indirect
@@ -235,6 +237,7 @@ require (
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- vendor/github.com/operator-framework/operator-registry/alpha/action/render.go
+++ vendor/github.com/operator-framework/operator-registry/alpha/action/render.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"path/filepath"
 	"sort"
@@ -15,6 +15,7 @@ import (
 
 	"github.com/h2non/filetype"
 	"github.com/h2non/filetype/matchers"
+	"github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/util/sets"
 
@@ -52,13 +53,14 @@ type Render struct {
 	Refs           []string
 	Registry       image.Registry
 	AllowedRefMask RefType
+	Migrate        bool
 
 	skipSqliteDeprecationLog bool
 }
 
 func nullLogger() *logrus.Entry {
 	logger := logrus.New()
-	logger.SetOutput(ioutil.Discard)
+	logger.SetOutput(io.Discard)
 	return logrus.NewEntry(logger)
 }
 
@@ -88,6 +90,12 @@ func (r Render) Run(ctx context.Context) (*declcfg.DeclarativeConfig, error) {
 			sort.Slice(b.RelatedImages, func(i, j int) bool {
 				return b.RelatedImages[i].Image < b.RelatedImages[j].Image
 			})
+		}
+
+		if r.Migrate {
+			if err := migrate(cfg); err != nil {
+				return nil, fmt.Errorf("migrate: %v", err)
+			}
 		}
 
 		cfgs = append(cfgs, *cfg)
@@ -147,7 +155,7 @@ func (r Render) imageToDeclcfg(ctx context.Context, imageRef string) (*declcfg.D
 	if err != nil {
 		return nil, err
 	}
-	tmpDir, err := ioutil.TempDir("", "render-unpack-")
+	tmpDir, err := os.MkdirTemp("", "render-unpack-")
 	if err != nil {
 		return nil, err
 	}
@@ -395,13 +403,54 @@ func moveBundleObjectsToEndOfPropertySlices(cfg *declcfg.DeclarativeConfig) {
 	}
 }
 
+func migrate(cfg *declcfg.DeclarativeConfig) error {
+	migrations := []func(*declcfg.DeclarativeConfig) error{
+		convertObjectsToCSVMetadata,
+	}
+
+	for _, m := range migrations {
+		if err := m(cfg); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func convertObjectsToCSVMetadata(cfg *declcfg.DeclarativeConfig) error {
+BundleLoop:
+	for bi, b := range cfg.Bundles {
+		if b.Image == "" || b.CsvJSON == "" {
+			continue
+		}
+
+		var csv v1alpha1.ClusterServiceVersion
+		if err := json.Unmarshal([]byte(b.CsvJSON), &csv); err != nil {
+			return err
+		}
+
+		props := b.Properties[:0]
+		for _, p := range b.Properties {
+			switch p.Type {
+			case property.TypeBundleObject:
+				// Get rid of the bundle objects
+			case property.TypeCSVMetadata:
+				// If this bundle already has a CSV metadata
+				// property, we won't mutate the bundle at all.
+				continue BundleLoop
+			default:
+				// Keep all of the other properties
+				props = append(props, p)
+			}
+		}
+		cfg.Bundles[bi].Properties = append(props, property.MustBuildCSVMetadata(csv))
+	}
+	return nil
+}
+
 func combineConfigs(cfgs []declcfg.DeclarativeConfig) *declcfg.DeclarativeConfig {
 	out := &declcfg.DeclarativeConfig{}
 	for _, in := range cfgs {
-		out.Packages = append(out.Packages, in.Packages...)
-		out.Channels = append(out.Channels, in.Channels...)
-		out.Bundles = append(out.Bundles, in.Bundles...)
-		out.Others = append(out.Others, in.Others...)
+		out.Merge(&in)
 	}
 	return out
 }
